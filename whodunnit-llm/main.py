a6fc2bbb61b1eb786b4f97cb32ba281508b74cd2
pierluigitrc <pierluigitrc@gmail.com>
2025-06-05 15:38:17 +0200

    [results] completed evaluation for llama3.3 70B

--- whodunnit-llm/main.py
+++ whodunnit-llm/main.py
@@ -120,15 +120,13 @@ def test(
         sleep(time_sleep)
 
 if __name__ == '__main__':
-    # si riparte da 17 per llama
-
     for csv_filename in sorted(get_only_one_case_episode()):
         test(
             episode = Episode(filename = str(csv_filename)), 
             n_scene_chunks = 4, 
-            platform = Platform.OPENROUTER_AI_API, 
-            model = OPENROUTER__DEEPSEEK_R1,
-            log_file = 'results_only_one_case_episodes_deepseek_r1.txt',
+            platform = Platform.GROQ_AI_API, 
+            model = GROQ__LLAMA_3_3_70B_VERSATILE,
+            log_file = 'results_only_one_case_episodes_llama33_70b.txt',
             time_sleep = 120)
 
 
