ddb5972bf225f1f0d2f3003702c0bbbdf2439109
pierluigitrc <pierluigitrc@gmail.com>
2025-08-31 16:00:03 +0200

    [results] new result validation

--- whodunnit-llm/results_validation.py
+++ whodunnit-llm/results_validation.py
@@ -79,39 +79,39 @@ def plot_perpetrators_identified(models, values, plot_title):
     plt.savefig(plot_title)
 
 co_star_prompt_result_folders = [
-    f'{RESULTS_PATH}deepseek-deepseek-r1_co_star_prompt/',
-    f'{RESULTS_PATH}google-gemini-2-0-flash-001_co_star_prompt/',
-    f'{RESULTS_PATH}meta-llama-llama-4-maverick_co_star_prompt/',
-    f'{RESULTS_PATH}openai-gpt-4-1-mini_co_star_prompt/',
+    f'{RESULTS_PATH}deepseek-deepseek-r1_co_star_instruction/',
+    f'{RESULTS_PATH}google-gemini-2-0-flash-001_co_star_instruction/',
+    f'{RESULTS_PATH}meta-llama-llama-4-maverick_co_star_instruction/',
+    f'{RESULTS_PATH}openai-gpt-4-1-mini_co_star_instruction/',
 ]
 
 open_ai_guidelines_result_folders = [
-    f'{RESULTS_PATH}deepseek-deepseek-r1_open_ai_guidelines_format/',
-    f'{RESULTS_PATH}google-gemini-2-0-flash-001_open_ai_guidelines_format/',
-    f'{RESULTS_PATH}meta-llama-llama-4-maverick_open_ai_guidelines_format/',
-    f'{RESULTS_PATH}openai-gpt-4-1-mini_open_ai_guidelines_format/',
+    f'{RESULTS_PATH}deepseek-deepseek-r1_open_ai_guidelines_instruction/',
+    f'{RESULTS_PATH}google-gemini-2-0-flash-001_open_ai_guidelines_instruction/',
+    f'{RESULTS_PATH}meta-llama-llama-4-maverick_open_ai_guidelines_instruction/',
+    f'{RESULTS_PATH}openai-gpt-4-1-mini_open_ai_guidelines_instruction/',
 ]
 
 co_star_prompt_source_retrieval = [
-    f'{RESULTS_PATH}deepseek-deepseek-r1_co_star_prompt_source_retrieval/',
-    f'{RESULTS_PATH}google-gemini-2-0-flash-001_co_star_prompt_source_retrieval/',
-    f'{RESULTS_PATH}meta-llama-llama-4-maverick_co_star_prompt_source_retrieval/',
-    f'{RESULTS_PATH}openai-gpt-4-1-mini_co_star_prompt_source_retrieval/'
+    f'{RESULTS_PATH}deepseek-deepseek-r1_co_star_instruction_source_retrieval/',
+    f'{RESULTS_PATH}google-gemini-2-0-flash-001_co_star_instruction_source_retrieval/',
+    f'{RESULTS_PATH}meta-llama-llama-4-maverick_co_star_instruction_source_retrieval/',
+    f'{RESULTS_PATH}openai-gpt-4-1-mini_co_star_instruction_source_retrieval/'
 ]
 
 
 def validation(folder: str, plot_title: str, prompt_type: str):
-    total_perpetrators = sum([len(', '.join(PERPETRATORS[k]).split(', ')) for k in PERPETRATORS.keys()])
     cases_solved: dict = {}
     coherence_indicator: dict = {}
 
     for model_results in folder:
         total_perpetrators_identified = 0
 
-        model = re.match(pattern = r'.+\/(.+)_(co_star_prompt\/|open_ai_guidelines_format\/|co_star_prompt_source_retrieval\/)', string = str(model_results)).group(1)
+        model = re.match(pattern = r'.+\/(.+)_(co_star_instruction\/|open_ai_guidelines_instruction\/|co_star_instruction_source_retrieval\/)', string = str(model_results)).group(1)
 
         for episode_filename in sorted(os.listdir(model_results)):
             ep_key = re.match(pattern = r'(s\d+e\d+).+', string = episode_filename).group(1)
+            print(f"{episode_filename} | {prompt_type} | checking episode: {ep_key}")
             
             perpetrators_first_iterations = []
             perpetrators_found = [] #only for last iteration
@@ -124,15 +124,15 @@ def validation(folder: str, plot_title: str, prompt_type: str):
 
                         if scene_chunk != None and perpetrator != None and perpetrator not in ['perpetrator']:
                             if scene_chunk != '3' and perpetrator not in perpetrators_first_iterations:
-                                perpetrators_first_iterations.append(str(perpetrator))
+                                perpetrators_first_iterations.append(str(perpetrator).lower())
                             elif scene_chunk == '3' and perpetrator not in perpetrators_found:
-                                perpetrators_found.append(str(perpetrator))
+                                perpetrators_found.append(str(perpetrator).lower())
 
             for p in ', '.join(PERPETRATORS[ep_key]).split(', '):
-                if p in perpetrators_found:
+                if p.lower() in perpetrators_found:
                     total_perpetrators_identified += 1
 
-                    if p in perpetrators_first_iterations:
+                    if p.lower() in perpetrators_first_iterations:
                         coherence_indicator[model] = coherence_indicator[model] + 1 if model in coherence_indicator.keys() else 1
             
         cases_solved[model] = total_perpetrators_identified
@@ -142,6 +142,6 @@ def validation(folder: str, plot_title: str, prompt_type: str):
     plot_coherence_indicator(list(coherence_indicator.keys()), list(coherence_indicator.values()), f"Coherence Indicator {prompt_type}")
 
 if __name__ == '__main__':
-    validation(folder = co_star_prompt_source_retrieval, plot_title = 'Perpetrators identified #2', prompt_type = '(CO_STAR prompt)')
-    # validation(folder = co_star_prompt_result_folders, plot_title = "Perpetrators identified", prompt_type = '(CO_STAR prompt)')
-    # validation(folder = open_ai_guidelines_result_folders, plot_title = "Perpetrators identified", prompt_type = '(OpenAI guidelines prompt)')
+    validation(folder = open_ai_guidelines_result_folders, plot_title = "Perpetrators identified", prompt_type = '(OpenAI guidelines)')
+    validation(folder = co_star_prompt_result_folders, plot_title = "Perpetrators identified", prompt_type = '(CO_STAR Framework)')
+    validation(folder = co_star_prompt_source_retrieval, plot_title = 'Perpetrators identified', prompt_type = '(CO_STAR Framework - with source retrieval)')
